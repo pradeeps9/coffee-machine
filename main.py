df00a751d2a7f32a3c9e9426c768594638cdecdf
Pradeep Bansiwal <46450532+pradeeps9@users.noreply.github.com>
2021-12-27 15:37:53 +0530

    coffee_machine_usingOOPS
    Same code different application is applied in this file they work almost same.

--- main.py
+++ main.py
@@ -1,104 +1,38 @@
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-            "milk": 0
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
-}
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-    "money": 0
-}
+menu = Menu()
+menuitem = MenuItem
 
-# toc = types of coins
-toc = {
-    "quarters": 25,
-    "dimes": 10,
-    "nickles": 5,
-    "penny": 1
-}
+coffeemaker = CoffeeMaker()
+moneymachine = MoneyMachine()
 
+machine_is_on = True
 
-# Main code
-def process_transaction(qu, di, ni, pe, ordered_coffee):
-    total_money = (qu*toc["quarters"] + di*toc["dimes"] + ni*toc["nickles"] + pe*toc["penny"])/100
-    orderamt = MENU[ordered_coffee]["cost"]
-    change = total_money - orderamt
-    if change < 0:
-        print("Sorry that's not enough money. Money refunded.")
-        return
-    elif change >= 0:
-        print(f"Here is ${change} in change")
-        make_coffee(ordered_coffee)
-        resources["money"] += orderamt
+while machine_is_on:
 
+    option = menu.get_items()
+    choice = input(f"What would you link {option}")
 
-def make_coffee(order):
-
-    resources["water"] -= MENU[order]["ingredients"]["water"]
-    resources["milk"] -= MENU[order]["ingredients"]["milk"]
-    resources["coffee"] -= MENU[order]["ingredients"]["coffee"]
-
-    print(f"Here is your {order} ☕. Enjoy!")
-
-
-def ingredients_finished(ordr):
-    if resources['water'] < MENU[ordr]['ingredients']['water']:
-        return True
-    elif resources['coffee'] < MENU[ordr]['ingredients']['coffee']:
-        return True
-    elif resources['milk'] < MENU[ordr]['ingredients']['milk']:
-        return True
+    if choice == 'off':
+        machine_is_on = False
+    elif choice == 'report':
+        coffeemaker.report()
+        moneymachine.report()
     else:
-        return False
+        drink = menu.find_drink(choice)
+        if coffeemaker.is_resource_sufficient(drink):
+            if moneymachine.make_payment(drink.cost):
+                coffeemaker.make_coffee(drink)
 
 
-coffee_machine_is_on = True
-while coffee_machine_is_on:
-
-    order = input("What would you like? (espresso/latte/cappuccino): ")
-
-    if order == "off":
-        coffee_machine_is_on = False
-    elif order == "report":
-        print(f"Water: {resources['water']}ml")
-        print(f"Milk: {resources['milk']}ml")
-        print(f"Coffee: {resources['coffee']}gm")
-        print(f"Money: ${resources['money']}")
-
-    elif order == 'espresso' or order == 'latte' or order == 'cappuccino':
-        if ingredients_finished(order):
-            print("Sorry there is not enough ingredients.")
-            continue
-        print("Please insert coins.")
-        q = int(input("how many quarters?: "))
-        d = int(input("how many dimes?: "))
-        n = int(input("how many nickles?: "))
-        p = int(input("how many pennies?: "))
-
-        process_transaction(q, d, n, p, order)
 
 
-        
+
+
+
+
+
+
+
